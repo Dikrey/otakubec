283eb820dc1748291cec0e6f14bc19c2098b3b73
Visualcodepo <156519759+Dikrey@users.noreply.github.com>
2026-03-03 12:19:43 +0700

    Refactor GET function to handle string ID

--- src/app/api/ongoing-anime/[id]/route.ts
+++ src/app/api/ongoing-anime/[id]/route.ts
@@ -1,8 +1,21 @@
+// import { NextResponse, NextRequest } from "next/server"
+// import ongoingAnime from "@/utils/ongoingAnime";
+
+// export async function GET(request: NextRequest, props: { params: Promise<{ id: number }> }) {
+//   const params = await props.params;
+//   const data = await ongoingAnime(params.id)
+//   return NextResponse.json({ data: data }, { status: 200 })
+// }
 import { NextResponse, NextRequest } from "next/server"
 import ongoingAnime from "@/utils/ongoingAnime";
 
-export async function GET(request: NextRequest, props: { params: Promise<{ id: number }> }) {
-  const params = await props.params;
-  const data = await ongoingAnime(params.id)
-  return NextResponse.json({ data: data }, { status: 200 })
+export async function GET(
+  request: NextRequest, 
+  context: { params: Promise<{ id: string }> }
+) {
+  const params = await context.params;
+  const idNumber = parseInt(params.id, 10);
+  const data = await ongoingAnime(idNumber);
+
+  return NextResponse.json({ data: data }, { status: 200 });
 }
