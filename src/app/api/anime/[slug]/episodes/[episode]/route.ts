fb27e2691d97902077828f3435e236523f2626bf
Visualcodepo <156519759+Dikrey@users.noreply.github.com>
2026-03-03 12:06:36 +0700

    Refactor GET function to use context parameters

--- src/app/api/anime/[slug]/episodes/[episode]/route.ts
+++ src/app/api/anime/[slug]/episodes/[episode]/route.ts
@@ -1,10 +1,29 @@
+// import { NextResponse, NextRequest } from "next/server"
+// import episode from "@/utils/episode";
+
+// export async function GET(request: NextRequest, props: { params: Promise<{ slug: string; episode: number }> }) {
+//   const params = await props.params;
+//   const urlParts = request.url.split("/")
+//   const animeSlug = urlParts[5]
+//   const data = await episode({ animeSlug: animeSlug, episodeNumber: params.episode })
+//   return NextResponse.json({ data: data }, { status: 200 })
+// }
+
+
 import { NextResponse, NextRequest } from "next/server"
 import episode from "@/utils/episode";
 
-export async function GET(request: NextRequest, props: { params: Promise<{ slug: string; episode: number }> }) {
-  const params = await props.params;
-  const urlParts = request.url.split("/")
-  const animeSlug = urlParts[5]
-  const data = await episode({ animeSlug: animeSlug, episodeNumber: params.episode })
-  return NextResponse.json({ data: data }, { status: 200 })
+export async function GET(
+  request: NextRequest, 
+  context: { params: Promise<{ slug: string; episode: string }> }
+) {
+  const params = await context.params;
+  const animeSlug = params.slug;
+  const episodeNumber = parseInt(params.episode, 10);
+  const data = await episode({ 
+    animeSlug: animeSlug, 
+    episodeNumber: episodeNumber 
+  });
+
+  return NextResponse.json({ data: data }, { status: 200 });
 }
