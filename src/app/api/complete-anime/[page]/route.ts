526557c44e01290856036df15226e64fe936b625
Visualcodepo <156519759+Dikrey@users.noreply.github.com>
2026-03-03 12:11:29 +0700

    Update route.ts

--- src/app/api/complete-anime/[page]/route.ts
+++ src/app/api/complete-anime/[page]/route.ts
@@ -1,8 +1,22 @@
+// import { NextResponse, NextRequest } from "next/server"
+// import completeAnime from "@/utils/completeAnime";
+
+// export async function GET(request: NextRequest, props: { params: Promise<{ page: number }> }) {
+//   const params = await props.params;
+//   const data = await completeAnime(params.page)
+//   return NextResponse.json({ data: data }, { status: 200 })
+// }
+
 import { NextResponse, NextRequest } from "next/server"
 import completeAnime from "@/utils/completeAnime";
 
-export async function GET(request: NextRequest, props: { params: Promise<{ page: number }> }) {
-  const params = await props.params;
-  const data = await completeAnime(params.page)
-  return NextResponse.json({ data: data }, { status: 200 })
+export async function GET(
+  request: NextRequest, 
+  context: { params: Promise<{ page: string }> }
+) {
+  const params = await context.params;
+  const pageNumber = Number(params.page);
+  const data = await completeAnime(isNaN(pageNumber) ? 1 : pageNumber);
+
+  return NextResponse.json({ data: data }, { status: 200 });
 }
